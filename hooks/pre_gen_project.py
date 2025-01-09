32ff85290b106645b839ac1029b50607da357a2e
RobinDeSmet <robin_de_smet@hotmail.be>
2025-01-09 23:20:30 +0100

    fix: pre gen hook

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,13 +4,5 @@ def transform_input(context):
     context["cookiecutter"]["package_name"] = transformed_name
 
 
-if __name__ == "__main__":
-    import os
-    import json
-
-    context_file = os.path.join(os.getcwd(), ".cookiecutter.context.json")
-    with open(context_file, "r") as f:
-        context = json.load(f)
+def main():
     transform_input(context)
-    with open(context_file, "w") as f:
-        json.dump(context, f)
