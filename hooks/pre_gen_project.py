7ca47be90b9e3404764e2f080014b89cfb6676fe
RobinDeSmet <robin_de_smet@hotmail.be>
2025-01-11 21:40:59 +0100

    feat: pre gen hook to update and validate project and package name

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,15 +1,23 @@
-def transform_input(context):
-    package_name = context["cookiecutter"]["package_name"]
-    project_name = context["cookiecutter"]["project_name"]
+import re
+import sys
 
-    # Transform the names: convert to lowercase and replace spaces with hyphens
-    transformed_package_name = package_name.lower().replace(" ", "-")
-    transformed_project_name = project_name.lower().replace(" ", "-")
+NAMING_CONVENTION_REGEX = r"^(?!\d)(?!.*__)(?!.*\.\.)[a-zA-Z_][a-zA-Z0-9_]*\.?(py)?$"
 
-    # Update the context with the transformed package name
-    context["cookiecutter"]["package_name"] = transformed_package_name
-    context["cookiecutter"]["project_name"] = transformed_project_name
+# Convert characters to lower case and change spaces to underscores
+"""
+{{ cookiecutter.update({"project_name": cookiecutter.project_name.lower().replace(" ", "_") }) }}
+{{ cookiecutter.update({"package_name": cookiecutter.package_name.lower().replace(" ", "_") }) }}
+"""
 
 
-def main(context):
-    transform_input(context)
+if re.match("{{cookiecutter.project_name}}", NAMING_CONVENTION_REGEX):
+    print(
+        "The project name does not fit the naming conventions. Please do not use special characters or numbers."
+    )
+    sys.exit(1)
+
+if re.match("{{cookiecutter.package_name}}", NAMING_CONVENTION_REGEX):
+    print(
+        "The package name does not fit the naming conventions. Please do not use special characters or numbers."
+    )
+    sys.exit(1)
