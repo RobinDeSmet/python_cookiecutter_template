02f050b391aa3b8b7cb4d8fdbfc68616c84448db
RobinDeSmet <robin_de_smet@hotmail.be>
2025-01-12 15:23:33 +0100

    feat: add data dir and fix pre gen validation checks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,21 +1,23 @@
 import re
 import sys
 
-NAMING_CONVENTION_REGEX = r"^(?!\d)(?!.*__)(?!.*\.\.)[a-zA-Z_][a-zA-Z0-9_]*\.?(py)?$"
+NAMING_CONVENTION_REGEX = r"^[a-z_][a-z0-9_]*$"
 
-print("Validating the variables...")
 
-# Validate the project slug and package slug
-if re.match("{{cookiecutter.__project_slug}}", NAMING_CONVENTION_REGEX):
-    print(
-        "The project name does not fit the naming conventions. Please do not use special characters or numbers."
-    )
-    sys.exit(1)
+if __name__ == "__main__":
+    print("Validating the variables...")
 
-if re.match("{{cookiecutter.__package_slug}}", NAMING_CONVENTION_REGEX):
-    print(
-        "The package name does not fit the naming conventions. Please do not use special characters or numbers."
-    )
-    sys.exit(1)
+    # Validate the project slug and package slug
+    if not bool(re.match("{{cookiecutter.__project_slug}}", NAMING_CONVENTION_REGEX)):
+        print(
+            "The project name does not fit the naming conventions. Please do not use special characters or numbers."
+        )
+        sys.exit(1)
 
-print("All variables ok! Proceeding to project creation...")
+    if not bool(re.match("{{cookiecutter.__package_slug}}", NAMING_CONVENTION_REGEX)):
+        print(
+            "The package name does not fit the naming conventions. Please do not use special characters or numbers."
+        )
+        sys.exit(1)
+
+    print("All variables ok! Proceeding to project creation...")
