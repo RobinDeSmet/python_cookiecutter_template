0356e3762b83e62e1a794368fe02bf0ba4553e10
RobinDeSmet <robin_de_smet@hotmail.be>
2025-01-12 16:04:48 +0100

    feat: create data dir after successful project creation

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 
-# Hook is run within the root directory of the project
 def remove_docker_files():
     dockerfile_path = Path("Dockerfile")
     dockercompose_path = Path("compose.yaml")
@@ -10,9 +9,16 @@ def remove_docker_files():
     dockercompose_path.unlink()
 
 
+def create_data_dir():
+    data_dir_gitkeep_path = Path("data")
+    data_dir_gitkeep_path.mkdir(parents=True, exist_ok=True)
+
+
 def main():
     if "{{ cookiecutter.include_docker_files }}" != "Yes":
         remove_docker_files()
+
+    create_data_dir()
 
     print("Project successfuly initialized!")
 
