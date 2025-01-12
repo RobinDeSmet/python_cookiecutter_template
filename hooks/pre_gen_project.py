9ba2a4a2d6fc525a22e3dc93d21de18ef562d8d3
RobinDeSmet <robin_de_smet@hotmail.be>
2025-01-12 15:58:05 +0100

    fix: pre gen hook

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -8,19 +8,19 @@ if __name__ == "__main__":
     print("Validating the variables...")
 
     # Validate the project slug and package slug
-    if not bool(re.match(NAMING_CONVENTION_REGEX, "{{cookiecutter.__project_slug}}")):
+    if not bool(re.match(NAMING_CONVENTION_REGEX, "{{cookiecutter.__package_slug}}")):
         print(
-            """The package name does not fit the naming conventions.
-            Please do not use special characters, other then '_', or numbers at the front of the name.
-            """
+            "The package name does not fit the naming conventions. "
+            " Please do not use special characters, other then '_', "
+            "or numbers at the front of the name."
         )
         sys.exit(1)
 
-    if not bool(re.match(NAMING_CONVENTION_REGEX, "{{cookiecutter.__package_slug}}")):
+    if not bool(re.match(NAMING_CONVENTION_REGEX, "{{cookiecutter.__project_slug}}")):
         print(
-            """The package name does not fit the naming conventions.
-            Please do not use special characters, other then '_', or numbers at the front of the name.
-            """
+            "The project name does not fit the naming conventions. "
+            " Please do not use special characters, other then '_', "
+            "or numbers at the front of the name."
         )
         sys.exit(1)
 
